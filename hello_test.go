1981482ccc9801734b6a7ea5fa06b48ba5d73998
PedroRevesMD <pedroreves11@gmail.com>
2025-08-12 23:42:26 -0300

    testing new test cases

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	text := Hello()
-	want := "Hello, World!"
+	text := Hello("Chris")
+	want := "Hello, Chris"
 
 	if text != want {
 		t.Errorf("Got %q want %q", text, want)

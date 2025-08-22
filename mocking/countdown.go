616e59912594f8d16ce1d17f86b49a6a55da1231
PedroRevesMD <pedroreves11@gmail.com>
2025-08-22 06:02:24 -0300

    add time between numbers

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const COUNTER = 3
@@ -11,6 +12,7 @@ const FINALMESSAGE = "Go!"
 func Countdown(out io.Writer) {
 	for i := COUNTER; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprintf(out, FINALMESSAGE)
